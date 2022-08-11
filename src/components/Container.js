475f8c468b20451a21eeb0614870d750607b51c0
Lucia Avanu <avanulucia.miruna@yahoo.com>
2022-08-11 13:21:00 +0300

    updated onClick

--- src/components/Container.js
+++ src/components/Container.js
@@ -5,22 +5,32 @@ export const Container = () => {
   let { publish } = PublishSubscribe();
   let counterValue = useCounter("modifyCounter");
 
-  function incrementEvent() {
-    publish("modifyCounter", counterValue + 1);
-  }
-
-  function decrementEvent() {
-    publish("modifyCounter", counterValue - 1);
+  function clickHandler(forwards = true) {
+    if (forwards) {
+      publish("modifyCounter", counterValue + 1);
+    } else {
+      publish("modifyCounter", counterValue - 1);
+    }
   }
 
   return (
     <div className="container">
       <p className="counter">{counterValue}</p>
       <div className="btns-container">
-        <button className="btn" onClick={incrementEvent}>
+        <button
+          className="btn"
+          onClick={() => {
+            clickHandler(true);
+          }}
+        >
           Increment
         </button>
-        <button className="btn" onClick={decrementEvent}>
+        <button
+          className="btn"
+          onClick={() => {
+            clickHandler(false);
+          }}
+        >
           Decrement
         </button>
       </div>
